ae562df77d3bc65b96c0d74bbea4f3451f50717f
tihico <timothy.hillenaar@gmail.com>
2026-01-05 09:51:38 +0100

    random dice roll added

--- medailleOpdracht/Program.cs
+++ medailleOpdracht/Program.cs
@@ -16,24 +16,31 @@ namespace medailleOpdracht
 
         static void Main(string[] args)
         {
-            
+            Random rand = new Random();
             bool admin = false;
-                Console.WriteLine("Als je een account wil maken schrijf een naam");
-            string userInput = Console.ReadLine();
-            Console.Clear();
-            Console.WriteLine("Nu type je password in");
-            string userInputPassword = Console.ReadLine();
-            Console.Clear();
-            Console.WriteLine("Nu typ in hoe oud je bent");
-            int userAge = Convert.ToInt32(Console.ReadLine());
-            if (userInput.ToLower() == "de ei man")
-            {
+            Console.WriteLine("Als je een account wil maken schrijf een naam");
+            for (int i = 0; i <= 100; i++)
+            
                 {
-                    admin = true;
+                    int result = 0;
+                    result = rand.Next(1, 7);
+                    Console.WriteLine(result);
                 }
-            }
-            User user1 = new User(userInput, userInputPassword, 25, 1, admin);
-            user1.Login();
+                string userInput = Console.ReadLine();
+                Console.Clear();
+                Console.WriteLine("Nu type je password in");
+                string userInputPassword = Console.ReadLine();
+                Console.Clear();
+                Console.WriteLine("Nu typ in hoe oud je bent");
+                int userAge = Convert.ToInt32(Console.ReadLine());
+                if (userInput.ToLower() == "de ei man")
+                {
+                    {
+                        admin = true;
+                    }
+                }
+                User user1 = new User(userInput, userInputPassword, 25, 1, admin);
+                user1.Login();
             }
              
         }
