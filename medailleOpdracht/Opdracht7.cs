0f2ca10442b904c4cf5140a75d34057f80e7c71b
tihico <timothy.hillenaar@gmail.com>
2026-01-09 12:06:48 +0100

    capitalization

--- medailleOpdracht/Opdracht7.cs
+++ medailleOpdracht/Opdracht7.cs
@@ -13,33 +13,33 @@ namespace medailleOpdracht
         public void Start()
         {
             Random rand = new Random();
-            float numberone = 0;
-            float numbertwo = 0;
-            float numberthree = 0;
-            float numberfour = 0;
-            float numberfive = 0;
-            float numbersix = 0;
-            int diceroll = 0;
-            float totaldice = 0;
+            float numberOne = 0;
+            float numberTwo = 0;
+            float numberThree = 0;
+            float numberFour = 0;
+            float numberFive = 0;
+            float numberSix = 0;
+            int diceRoll = 0;
+            float totalDice = 0;
             for (int i = 1; i <= 100; i++)
             {
-                diceroll = rand.Next(1, 7);
-                if (diceroll == 1) { numberone = numberone + 1; }
-                if (diceroll == 2) { numbertwo = numbertwo + 1; }
-                if (diceroll == 3) { numberthree = numberthree + 1; }
-                if (diceroll == 4) { numberfour = numberfour + 1; }
-                if (diceroll == 5) { numberfive = numberfive + 1; }
-                if (diceroll == 6) { numbersix = numbersix + 1; }
-                Console.WriteLine(diceroll);
+                diceRoll = rand.Next(1, 7);
+                if (diceRoll == 1) { numberOne = numberOne + 1; }
+                if (diceRoll == 2) { numberTwo = numberTwo + 1; }
+                if (diceRoll == 3) { numberThree = numberThree + 1; }
+                if (diceRoll == 4) { numberFour = numberFour + 1; }
+                if (diceRoll == 5) { numberFive = numberFive + 1; }
+                if (diceRoll == 6) { numberSix = numberSix + 1; }
+                Console.WriteLine(diceRoll);
             }
-            Console.WriteLine($" 1 has been written {numberone} times");
-            Console.WriteLine($" 2 has been written {numbertwo} times");
-            Console.WriteLine($" 3 has been written {numberthree} times");
-            Console.WriteLine($" 4 has been written {numberfour} times");
-            Console.WriteLine($" 5 has been written {numberfive} times");
-            Console.WriteLine($" 6 has been written {numbersix} times");
-            totaldice = (numberone + numbertwo*2 + numberthree*3 + numberfour*4 + numberfive*5 + numbersix*6) / 100;
-            Console.WriteLine($"The average of all dice rolls added up is {totaldice}");
+            Console.WriteLine($" 1 has been written {numberOne} times");
+            Console.WriteLine($" 2 has been written {numberTwo} times");
+            Console.WriteLine($" 3 has been written {numberThree} times");
+            Console.WriteLine($" 4 has been written {numberFour} times");
+            Console.WriteLine($" 5 has been written {numberFive} times");
+            Console.WriteLine($" 6 has been written {numberSix} times");
+            totalDice = (numberOne + numberTwo*2 + numberThree*3 + numberFour*4 + numberFive*5 + numberSix*6) / 100;
+            Console.WriteLine($"The average of all dice rolls added up is {totalDice}");
         }
 
     }
