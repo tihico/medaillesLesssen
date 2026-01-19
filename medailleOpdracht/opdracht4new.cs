a645611ab6363f2fd090cabe8221df8f49a6b43f
tihico <timothy.hillenaar@gmail.com>
2026-01-19 05:15:53 +0100

    opdracht4 klaar

--- medailleOpdracht/opdracht4new.cs
+++ medailleOpdracht/opdracht4new.cs
@@ -10,22 +10,38 @@ namespace medailleOpdracht
     {
         public void Start()
         {
+            int currentYear = 2026;
+            int leeftijd = 0;
             Console.WriteLine("What year were you born?");
+
             int geboorteJaar = Convert.ToInt32(Console.ReadLine());
+            leeftijd = currentYear - geboorteJaar;
             Console.Clear();
             Console.WriteLine("Has it already been your birthday?");
             Console.WriteLine("Yes");
             Console.WriteLine("No");
             string Birthday = Console.ReadLine();
+            if(Birthday.ToLower() == "yes" && geboorteJaar >= 18 ) { Console.WriteLine("Je mag stemmen"); }
             if (Birthday.ToLower() == "yes")
             {
-                Console.WriteLine($"Je bent {2026 - geboorteJaar}");
+                Console.WriteLine($"Je bent {leeftijd}");
             }
             else if (Birthday.ToLower() == "no")
             {
-                Console.WriteLine($"Je bent {2026 - geboorteJaar - 1}");
+                leeftijd -= 1;
+                Console.WriteLine($"Je bent {leeftijd}");
             }
+            CheckVotingAge(leeftijd);
+        }
+
+        public void CheckVotingAge(int leeftijd)
+        {
+            if (leeftijd >= 18)
+            {
+                Console.WriteLine("Je mag stemmen");
             }
+            else if(leeftijd < 18) { Console.WriteLine("Je mag niet stemmen"); }
         }
-        }
+    }
+}
     
